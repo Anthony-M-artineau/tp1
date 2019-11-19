812754065fc87df0d776af5d7f69295178825baf
ANMAR570 <martineaua99@gmail.com>
2019-11-18 20:23:59 -0500

    4e commit

--- api.py
+++ api.py
@@ -1,20 +1,20 @@
 import requests
 
-url_base = 'https://python.gel.ulaval.ca/quoridor/api/'
+urlBase = 'https://python.gel.ulaval.ca/quoridor/api/'
 def lister_parties(idul):
     """Lister"""
     url = 'lister/'
-    rep = requests.get(url_base+url, params={'idul': idul})
+    rep = requests.get(urlBase+url, params={'idul': idul})
     return retourne_requests(rep, url)
 def débuter_partie(idul):
     """"Débuter"""
     url = 'débuter/'
-    rep = requests.post(url_base+url, data={'idul': idul})
+    rep = requests.post(urlBase+url, data={'idul': idul})
     return retourne_requests(rep, url)
 def jouer_coup(idul):
     """Jouer"""
     url = 'jouer/'
-    rep = requests.post(url_base+url, data={'idul': idul})
+    rep = requests.post(urlBase+url, data={'idul': idul})
     return retourne_requests(rep, url)
 def retourne_requests(rep, url):
     """Request"""
@@ -22,5 +22,4 @@ def retourne_requests(rep, url):
         # la requête s'est déroulée normalement; décoder le JSON
         rep = rep.json()
         return rep
-    else:
-        print(f"Le GET sur {url_base+url} a produit le code d'erreur {rep.status_code}.")
+
--- main.py
+++ main.py
@@ -8,15 +8,4 @@ def analyser_commande():
     parser = argparse.ArgumentParser(description='Jeu Quoridor - phase 1')
     parser.add_argument('idul', help='IDUL du joueur.')
     parser.add_argument('-l', '--lister', metavar='', help='Lister les identifiants de vos 20 dernères parties.')
-    afficher_damier_ascii(api.débuter_partie(parser.parse_args().idul))
     return parser.parse_args()
-
-
-#afficher un damier en art ascii
-def afficher_damier_ascii():
-
-
-
-#demander à l'utilisateur de spécifier son coup
-#transmettre le coup au seveur
-#repartir ligne 6
