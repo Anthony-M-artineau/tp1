3c84a9d6ed4c790a9cd385a6019a66853de6e819
ANMAR570 <martineaua99@gmail.com>
2019-11-18 20:34:47 -0500

    6e commit

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import argparse 
-#contacter le serveur
-import api
+
 
 #faire une commande help
 def analyser_commande():
     """Commande"""
     parser = argparse.ArgumentParser(
         description='Jeu Quoridor - phase 1'
-)
+        )
     parser.add_argument(
         'idul', help='IDUL du joueur.'
-)
+        )
     parser.add_argument(
         '-l', '--lister', metavar='', help='Lister les identifiants de vos 20 dernères parties.'
-)
+        )
     return parser.parse_args()
